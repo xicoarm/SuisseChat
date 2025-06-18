d3a744807a8a4b86120280dbe1a91c4c07c88404
xicoarm <areg.arakelyan@suisse-ai.ch>
2025-06-18 13:44:38 +0200

    log2

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,9 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
+      : '[SuisseGPT ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        '](https://suisse-ai.ch) - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
